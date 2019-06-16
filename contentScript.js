f8b5afc26abd437c0bbde97a1e300be52400a670
Ben Black <benjyblack@gmail.com>
2019-06-16 18:05:57 -0400

    Add button to play on spotify

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,19 @@
-const titleEl = document.querySelector('h1');
-const artistEl = titleEl.parentElement.querySelector('a');
+const STOP_WORDS = ['chords', 'tab'];
 
-const title = titleEl.textContent.replace(/chords/, '').trim();
+const titleEl = document.querySelector('h1');
+const headerEl = titleEl.parentElement;
+const artistEl = headerEl.querySelector('a');
+
+const ignoredTitleWordsRegex = new RegExp(`${STOP_WORDS.join('|')}`, '');
+const title = titleEl.textContent.replace(ignoredTitleWordsRegex, '').trim();
 const artist = artistEl.textContent;
 
-alert(`${title} by ${artist}`);
+const playButton = document.createElement('button');
+const playText = document.createTextNode('Play on Spotify');
+playButton.appendChild(playText);
+
+headerEl.appendChild(playButton);
+
+playButton.addEventListener('click', function(){
+  alert(`${title} by ${artist}`);
+});
