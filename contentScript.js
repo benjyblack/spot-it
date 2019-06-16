8d812f73a2576e903f05b07f2a70a7b2020c2756
Ben Black <benjyblack@gmail.com>
2019-06-16 17:54:44 -0400

    Add basic text parsing

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,7 @@
-alert('We\'re here!');
+const titleEl = document.querySelector('h1');
+const artistEl = titleEl.parentElement.querySelector('a');
+
+const title = titleEl.textContent.replace(/chords/, '').trim();
+const artist = artistEl.textContent;
+
+alert(`${title} by ${artist}`);
